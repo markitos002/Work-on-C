330f8a6496c30f31a4b4e27bf187d4501cf54d64
Marco Avila <markos.avila@gmail.com>
2021-06-06 21:14:24 -0500

    se saco la carpeta de practicas del libro Oreilly

--- oreilly/pracc/mk-all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/csh
-foreach x (*)
-  if (-d $x) then
-     echo "-------------- $x -----------------"
-     (cd $x;make -f makefile.gcc clean;make -f makefile.gcc)
-  endif
-end
--- oreilly/pracc/ret/square.c
+++
@@ -1,5 +0,0 @@
-float square(s)
-int s;
-{
-    return (s * s);
-}
